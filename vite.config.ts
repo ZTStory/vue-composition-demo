ea5ddf2e8f662da3f3d00b5c8a2978be8a8809d3
ZTStory <m18740395237@163.com>
2022-07-01 16:42:59 +0800

    🐞 fix: ci

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: process.env.NODE_ENV === "production" ? "/vue-keepalive-clear/" : "/",
+    build: {
+        outDir: "dist",
+    },
     plugins: [vue()],
     // 配置别名
     resolve: {
