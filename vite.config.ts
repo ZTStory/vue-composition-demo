b79e101e9688ebfb6ae3a5cbd5d70ab1eeb25f87
ZTStory <m18740395237@163.com>
2022-07-03 23:34:35 +0800

    🐞 fix(pgk.json name && vite.config path):

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: process.env.NODE_ENV === "production" ? "/vue-keepalive-clear/" : "/",
+    base: process.env.NODE_ENV === "production" ? "/vue-composition-demo/" : "/",
     build: {
         outDir: "dist",
     },
