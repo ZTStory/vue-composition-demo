eaaa6943729f7341a955284b680ab2bd346f066c
liuzt50906 <liuzt50906@hundsun.com>
2024-01-10 14:51:47 +0800

    🐞 fix(首页白屏):

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "@/utils/auto-update";
 
 const app = createApp(App);
 app.use(router);
--- src/pages/auto-update/index.vue
+++ src/pages/auto-update/index.vue
@@ -1,12 +1,15 @@
 <template>
-    <div>已开启页面更新监听，具体查看 console 面板</div>
+  <div>已开启页面更新监听，具体查看 console 面板</div>
 </template>
 
 <script lang="ts">
 export default {
-    name: "AutoUpdate",
+  name: "AutoUpdate",
 };
 </script>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import "@/utils/auto-update";
+</script>
 
 <style scoped></style>
+
